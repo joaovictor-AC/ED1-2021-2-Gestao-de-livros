fc182b00337b242481af4a4f153412e5fde1529f
João Victor Araújo <jvaraujocosta13@gmail.com>
2022-03-16 15:08:19 -0300

    Fixing Details

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Book.h"
 
 int main() {
 	
 	Book *b = newBook(145, "Harry Potter", "Abril", 278, "J.K Rowling", 23/02/98);
 	printf("#%d-%s\n",getBookId(b),getBookTitle(b));
+	free(b);
 	
 }
